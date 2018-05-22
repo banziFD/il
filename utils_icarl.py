8c7cc8c2e21792e122fb58c1681fb9ed1185e43c
banziFD <supengyumail@gmail.com>
2018-05-22 11:00:20 -0400

    fix bugs on icarl.choosetop

--- utils_icarl.py
+++ utils_icarl.py
@@ -169,12 +169,8 @@ class iCaRL(torch.nn.Module):
             self.unknown[known_cl] = 0
     
     # Pick up top-nb_proto images as example of typical class
-<<<<<<< HEAD
-    def choose_top_(self, nb_proto, feature_mem, image_mem, image_orig_mem, class_mean):
-=======
     def choose_top_(self, nb_proto, feature_mem, 
     image_mem, image_orig_mem, class_mean):
->>>>>>> 75e846a50e9f9ee1fc716760d20895ff32e0e83d
         assert feature_mem.shape[0] == image_mem.shape[0]
         distance = feature_mem - class_mean
         distance = distance * distance
@@ -188,35 +184,26 @@ class iCaRL(torch.nn.Module):
         assert feature_mem.shape[0] == image_mem.shape[0]
         visited = torch.zeros(feature_mem.shape[0], dtype = torch.uint8)
         tot = torch.zeros(feature_mem.shape[1])
-<<<<<<< HEAD
         if(self.gpu):
             tot = tot.cuda()
-=======
->>>>>>> 75e846a50e9f9ee1fc716760d20895ff32e0e83d
         protoset_index = []
         for i in range(nb_proto):
             distance = torch.tensor(float('inf'))
             p = -1
             for item in range(feature_mem.shape[0]):
-                if(visited[item] != 0):
+                if(visited[item] == 0):
                     avg = (tot + feature_mem[i]) / (i + 1)
                     distance_temp = torch.norm(class_mean - avg)
                     if(distance_temp < distance):
-                        p = i
+                        p = item
             protoset_index.append(p)
-<<<<<<< HEAD
+            visited[p] = 1
             tot = tot + feature_mem[p]
-=======
->>>>>>> 75e846a50e9f9ee1fc716760d20895ff32e0e83d
         index = torch.tensor(protoset_index)
         protoset = image_mem[index].clone()
         protoset_orig = image_orig_mem[index].clone()
         return [protoset, protoset_orig, class_mean]
-<<<<<<< HEAD
-    
-=======
                
->>>>>>> 75e846a50e9f9ee1fc716760d20895ff32e0e83d
     def construct_proto(self, iter_group, mixing, loader, protoset):
         # Protoset will be a dictionary of tuples, where keys are class labels, values are tuple of (image, image_orig, feature_mean
 
